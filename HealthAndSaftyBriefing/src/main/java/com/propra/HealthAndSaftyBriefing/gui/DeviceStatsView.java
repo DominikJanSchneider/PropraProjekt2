ee4cf69e216c0e7d3dc467291841217c06072c02
LeSch1997 <leon.schmid@student.uni-siegen.de>
2020-07-13 16:34:13 +0200

    Filters in UserView added

--- HealthAndSaftyBriefing/src/main/java/com/propra/HealthAndSaftyBriefing/gui/DeviceStatsView.java
+++ HealthAndSaftyBriefing/src/main/java/com/propra/HealthAndSaftyBriefing/gui/DeviceStatsView.java
@@ -24,10 +24,12 @@ import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.BeforeEvent;
 import com.vaadin.flow.router.HasUrlParameter;
+import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 @SuppressWarnings("serial")
 @Route("DeviceStatsView")
+@PageTitle("Ger\u00e4testatistik | Sicherheitsunterweisung")
 public class DeviceStatsView extends VerticalLayout implements HasUrlParameter<String> {
 	private DeviceStatsManager deviceStatsM;
 	private TextField tfSearch;
--- HealthAndSaftyBriefing/src/main/java/com/propra/HealthAndSaftyBriefing/gui/UserView.java
+++ HealthAndSaftyBriefing/src/main/java/com/propra/HealthAndSaftyBriefing/gui/UserView.java
@@ -1,18 +1,24 @@
 package com.propra.HealthAndSaftyBriefing.gui;
 
 
+import java.util.Comparator;
 import java.util.List;
-import java.util.NoSuchElementException;
 import com.propra.HealthAndSaftyBriefing.authentication.AccessControl;
 import com.propra.HealthAndSaftyBriefing.authentication.AccessControlFactory;
 import com.propra.HealthAndSaftyBriefing.backend.DeviceManager;
 import com.propra.HealthAndSaftyBriefing.backend.UserManager;
 import com.propra.HealthAndSaftyBriefing.backend.data.AssignedDevice;
+import com.propra.HealthAndSaftyBriefing.backend.data.Device;
 import com.propra.HealthAndSaftyBriefing.backend.data.Person;
 import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.FocusNotifier;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.KeyModifier;
+import com.vaadin.flow.component.ShortcutRegistration;
+import com.vaadin.flow.component.BlurNotifier.BlurEvent;
+import com.vaadin.flow.component.FocusNotifier.FocusEvent;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.details.Details;
 import com.vaadin.flow.component.grid.Grid;
@@ -22,6 +28,8 @@ import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
@@ -54,6 +62,10 @@ public class UserView extends VerticalLayout {
 	private TextField tfEnd;
 	private TextField tfEMail;
 	private Person userData;
+	private TextField tfSearch;
+	private Button btnSearch;
+	protected ShortcutRegistration shortReg;
+	private Tabs searchTabs;
 	
 	public UserView() {
 		accessControl = AccessControlFactory.getInstance().createAccessControl();
@@ -85,7 +97,7 @@ public class UserView extends VerticalLayout {
 		btnSetUsageTime = new Button("Zeit Setzen");
 		btnSetUsageTime.addClickListener(e -> setUsageTime());
 		HorizontalLayout usageTimeLayout = new HorizontalLayout(tfUsageTime, btnSetUsageTime);
-		VerticalLayout timeSettingsContent = new VerticalLayout(usageTimeLayout, userDeviceGrid);
+		VerticalLayout timeSettingsContent = new VerticalLayout(configureSearchComponents(), userDeviceGrid, usageTimeLayout);
 		Details timeSettings = new Details("Nutzungszeiteinstellungen", timeSettingsContent);
 		return timeSettings;
 	}
@@ -147,7 +159,7 @@ public class UserView extends VerticalLayout {
 						.setKey("id")
 						.setSortable(true);
 		userDeviceGrid.addColumn(AssignedDevice::getName)
-						.setHeader("Ger\u00e4t")
+						.setHeader("Name")
 						.setKey("name")
 						.setSortable(true);
 		userDeviceGrid.addColumn(AssignedDevice::getDescription)
@@ -190,6 +202,41 @@ public class UserView extends VerticalLayout {
 		return briefingInformation;
 	}
 	
+	private Component configureSearchComponents() {
+		tfSearch = new TextField();
+		btnSearch = new Button("Suchen");
+		btnSearch.setIcon(VaadinIcon.SEARCH.create());
+		btnSearch.addClickListener(e -> searchPressed());
+		tfSearch.setWidth("200px");
+		tfSearch.setPlaceholder("Suche");
+		tfSearch.setAutoselect(true);
+		tfSearch.addFocusListener(new ComponentEventListener<FocusNotifier.FocusEvent<TextField>>() {
+
+			@Override
+			public void onComponentEvent(FocusEvent<TextField> event) {
+				shortReg = btnSearch.addClickShortcut(Key.ENTER);
+			}
+			
+		});
+		tfSearch.addBlurListener(new ComponentEventListener<BlurEvent<TextField>>() {
+
+			@Override
+			public void onComponentEvent(BlurEvent<TextField> event) {
+				shortReg.remove();
+			}
+			
+		});
+		Tab idTab = new Tab("Ger\u00e4teID");
+		Tab nameTab = new Tab("Name");
+		Tab roomTab = new Tab("Raum");
+		searchTabs = new Tabs(idTab, nameTab, roomTab);
+		VerticalLayout searchComponent1 = new VerticalLayout(tfSearch, btnSearch);
+		Label label = new Label("Suchen nach:");
+		label.addComponentAsFirst(VaadinIcon.FILTER.create());
+		VerticalLayout searchComponent2 = new VerticalLayout(label , searchTabs);
+		return new HorizontalLayout(searchComponent1, searchComponent2);
+	}
+	
 	private void updateUserInfo() {
 		tfDate.setValue(userData.getDate());
 		tfIfwt.setValue(userData.getIfwt());
@@ -206,19 +253,42 @@ public class UserView extends VerticalLayout {
 	}
 	
 	private void updateUserDeviceGrid() {
-		userData = userM.getUserData(accessControl.getPrincipalName());
 		List<AssignedDevice> devices = deviceM.getAssignedDevices(userData.getId());
 		userDeviceGrid.setItems(devices);
 	}
 	
+	private void updateUserDeviceGridByID(int dID) {
+		List<AssignedDevice> devices = deviceM.getAssignedDevicesByID(userData.getId(), dID);
+		devices.sort(Comparator.comparing(Device::getId));
+        userDeviceGrid.setItems(devices);
+	}
+	
+	private void updateUserDeviceGridByName(String name) {
+		List<AssignedDevice> devices = deviceM.getAssignedDevicesByName(userData.getId(), name);
+		devices.sort(Comparator.comparing(Device::getId));
+		userDeviceGrid.setItems(devices);
+	}
+	
+	private void updateUserDeviceGridByRoom(String room) {
+		List<AssignedDevice> devices = deviceM.getAssignedDevicesByRoom(userData.getId(), room);
+		devices.sort(Comparator.comparing(Device::getId));
+		userDeviceGrid.setItems(devices);
+	}
+	
 	private void setUsageTime() {
-		try {
+		AssignedDevice selectedDevice = userDeviceGrid.asSingleSelect().getValue();
+		if(selectedDevice != null) {
 			int userID = userData.getId();
-			int dID = userDeviceGrid.getSelectionModel().getFirstSelectedItem().get().getId();
-			double usageTime = Double.parseDouble(tfUsageTime.getValue());
-			deviceM.setUsageTime(dID, userID, usageTime);
-			updateUserDeviceGrid();
-		} catch (NoSuchElementException e) {
+			int dID = selectedDevice.getId();
+			try {
+				double usageTime = Double.parseDouble(tfUsageTime.getValue());
+				deviceM.setUsageTime(dID, userID, usageTime);
+				updateUserDeviceGrid();
+			} catch (NumberFormatException e) {
+				Notification.show("Bitte geben Sie eine Zahl ein.");
+			}
+		}
+		else {
 			Notification.show("Bitte w\u00e4hlen Sie ein Ger\u00e4t aus der Tabelle.");
 		}
 	}
@@ -234,5 +304,32 @@ public class UserView extends VerticalLayout {
 		// User can quickly activate logout with Ctrl+L
 		attachEvent.getUI().addShortcutListener(() -> logout(), Key.KEY_L, KeyModifier.CONTROL);
 		
+	}
+	
+	private void searchPressed() {
+		String tabName = searchTabs.getSelectedTab().getLabel();
+		String searchTxt = tfSearch.getValue();
+		switch(tabName) {
+			case "GeräteID":
+				try {
+					if(searchTxt.isEmpty()) {
+						updateUserDeviceGrid();
+					}
+					else {
+						updateUserDeviceGridByID(Integer.parseInt(searchTxt));
+					}
+				}
+				catch(NumberFormatException e) {
+					Notification.show("Bitte geben Sie eine Zahl ein.");
+				}
+				
+				break;
+			case "Name":
+				updateUserDeviceGridByName(searchTxt);
+				break;
+			case "Raum":
+				updateUserDeviceGridByRoom(searchTxt);
+				break;
+		}
 	}
 }
